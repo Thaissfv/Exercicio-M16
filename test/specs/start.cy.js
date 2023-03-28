43f1023c707641fbc4c6a1c22d9c19fda81727f4
Thais <thaissfurtado@gmail.com>
2023-03-28 17:07:39 -0300

    Inserindo a função de resultado

--- test/specs/start.cy.js
+++ test/specs/start.cy.js
@@ -1,18 +1,24 @@
 describe('Primeiro Teste', () => {
+        
     it('Acessar o menu forms', async () => {
 
         const seletor = 'new UiSelector().text("This app is awesome").className("android.widget.CheckedTextView")'
         const botao_frase = await $(`android=${seletor}`)
-
+        const actualText = 'WEBDRIVER'
+      
 
         await $('~Forms').click()
+        
         await $('~text-input').setValue('Exercício Módulo 16')
         expect (await $('~text-input')).toBeDisplayed()
+        
         await $('~Dropdown').click()
         await botao_frase.click()
-        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1,5)');
+        
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1,5)')
         await $('~button-Active').click()
-                
+        expect (await $('//android.widget.TextView').getText(actualText))
+        
     });
     
 });
